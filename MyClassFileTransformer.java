7015f7b6aa92e4b3fe3ba181573311abba390996
Jitin Paul <Jitin Paul>
2017-03-28 23:56:05 -0500

    ClassFileTransformer.java updated.

--- MyClassFileTransformer.java
+++ MyClassFileTransformer.java
@@ -14,19 +14,16 @@ public class MyClassFileTransformer implements ClassFileTransformer{
     @Override
     public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException 
     {
-            if ((s.startsWith("org/joda/time")) && !("org/joda/time/Listener".equals(s))) 
-			//if ("org/joda/time/Period".equals(s))
-			{
-            // ASM Code
-			System.out.println("Entered");
+			//if(s.startsWith("org/joda/time"))
+            if(s.startsWith("org/apache/commons/dbutils"))				
+			{	
             ClassReader reader = new ClassReader(bytes);
-            ClassWriter writer = new ClassWriter(reader, 0);
+            ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_MAXS);
             ClassPrinter visitor = new ClassPrinter(writer);
             reader.accept(visitor, 0);
             return writer.toByteArray();
-			}
-			
-			return null;
+			}			
+			return bytes;
     }
 
 }
