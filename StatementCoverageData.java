c117834a64865cf9345246a9624f05c93dc6698c
Jitin Paul <Jitin Paul>
2017-03-28 23:54:05 -0500

    StatementCoverageData.java updated

--- StatementCoverageData.java
+++ StatementCoverageData.java
@@ -1,33 +1,56 @@
 package TestCompetition.JavaAgent;
 
-//import java.util.HashMap;
-//import java.util.Iterator;
-//import java.util.Map;
-//import java.util.Set;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.io.*;
+import java.util.*;
 
 public class StatementCoverageData {
-	//static final Map<String, Integer> coverageHashmap = new HashMap<String, Integer>();
-
-	public static void MarkLineAsExecuted(int lineNumber, String className) {
-		  String str = String.format("%s.%d", className, lineNumber);
-	/*	try {
-		  //File file = new File("cov.txt");
-      
-         // creates the file
-          //file.createNewFile();
-		  FileWriter writer = new FileWriter("stmt-cov.txt",true); 
-      
-         // Writes the content to the file
-          writer.write(str);
-          writer.write(System.getProperty("line.separator")); 	 		  
-          writer.flush();
-          writer.close();
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}*/
-		
+	static String key;
+	static final Set<String> records = new HashSet<String>();
+	static final HashMap<String,Set<String>> h = new HashMap<>();
+	public static void testExecuted(String className, String methodName) 
+	{
+		  key = "[TEST] "+className+":"+methodName;
+		  h.put(key,new HashSet<>());;
 	}
 	
+	public static void lineExecuted(int lineNumber, String className) 
+	{
+		  String str = className+":"+lineNumber;
+		  str=str.replace('/', '.');
+		  if(h.get(key)!=null)
+		     h.get(key).add(str);
+	}
+	
+	public static void writeIntoFile()
+	{
+		try 
+		{
+		  File file = new File("stmt-cov.txt");
+		  if (file.exists())
+               file.delete();
+          else		   
+		       file.createNewFile();
+		  FileWriter writer = new FileWriter("stmt-cov.txt",true); 		
+		  for(Map.Entry<String, Set<String>> e : h.entrySet())
+		  {
+				writer.write(e.getKey());
+				writer.write(System.getProperty("line.separator"));
+				for(String temp : e.getValue())
+				{
+					writer.write(temp);
+					writer.write(System.getProperty("line.separator"));
+				}
+				    writer.write(System.getProperty("line.separator"));
+		  }
+       		  		  
+		  writer.close();
+		}
+		catch (Exception ex) 
+		{
+			ex.printStackTrace();
+	
+		}
+	}		
 }
