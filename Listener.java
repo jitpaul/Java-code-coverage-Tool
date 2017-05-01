8f37ed4b166770decf2f2f4fa6e8cf06ef176bce
Jithin Paul <paul.jitin@gmail.com>
2017-05-01 18:23:02 -0500

    Revert

--- Listener.java
+++ Listener.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package org.apache.commons.dbutils;
+package org.joda.time;
  
 import java.io.*;
 import junit.framework.*;
@@ -23,31 +23,46 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 import TestCompetition.JavaAgent.StatementCoverageData;
-import TestCompetition.JavaAgent.GenerateTestSuiteForJUnit4;
 
 public class Listener extends RunListener
 {
+	static FileWriter writer;
 	
-	//  Called when all tests have finished
+	public void testRunStarted(Description description) throws java.lang.Exception 
+	{
+        try 
+		{
+		  File file = new File("stmt-cov.txt");
+		  if (file.exists())
+               file.delete();
+          else		   
+		       file.createNewFile();
+		  writer = new FileWriter("stmt-cov.txt",false);
+		}
+		catch (Exception ex) 
+		{
+			ex.printStackTrace();
+	
+		}
+    }	
+	/**
+	 *  Called when all tests have finished
+	 * */
 	public void testRunFinished(Result result) throws java.lang.Exception
 	{
-		GenerateTestSuiteForJUnit4.generate("T_TestSuite", StatementCoverageData.totalPriorIntoFile());
-		GenerateTestSuiteForJUnit4.generate("A_TestSuite", StatementCoverageData.additionalPriorIntoFile());
+		writer.close();
 	}
+	
+	public void testFinished(Description description) throws java.lang.Exception
+	{
+        StatementCoverageData.writeIntoFile(writer);
+    }
 
-	//  Called when an atomic test is about to be started.	 
+	/**
+	 *  Called when an atomic test is about to be started.
+	 * */
 	public void testStarted(Description description) throws java.lang.Exception
 	{
-		 StatementCoverageData.testExecuted(description.getClassName(), description.getMethodName()); 
+		 writer.write("[TEST] "+description.getClassName()+":"+description.getMethodName() + System.lineSeparator());
 	}
-	
-	public void testFinished(Description description) throws Exception 
-	{
-        //StatementCoverageData.writeIntoFile();
-    }
-	
-	 public void testFailure(Failure failure) throws Exception 
-	{
-        System.exit(0);
-    }
 }
